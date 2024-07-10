c05858643e931ba0ddd72e90a4f9d3c7077d1103
arierabr <rierabrellalfons@gmail.com>
2024-07-10 19:27:13 +0200

    work 10 julio

--- functions.py
+++ functions.py
@@ -6,33 +6,33 @@ def searcher(json):
     # Create a dictionary to hold query parameters:
 
     params_1 = {
-        'fly_from': json["Origin"][1],
-        'date_from': json["Departure_d"][1],
-        'date_to': json["Arrivals_d"][1],
-        'dtime_from': json["Departures_h"][1],
+        'fly_from': json["Origin"][0],
+        'date_from': json["Departure_d"][0],
+        'date_to': json["Arrivals_d"][0],
+        'dtime_from': json["Departures_h"][0],
         'dtime_to': "23:00",
-        'return_from': json["Origin"][1],
-        'return_to': json["Arrivals_h"][1],
+        'return_from': json["Origin"][0],
+        'return_to': json["Arrivals_h"][0],
         'ret_atime_from': "06:00",
-        'ret_atime_to': json["Arrivals_h"][1],
-        'adults': json["Travelers"][1],
-        'max_stopovers': json["Origin"][1],
+        'ret_atime_to': json["Arrivals_h"][0],
+        'adults': json["Travelers"][0],
+        'max_stopovers': json["Origin"][0],
         'one_for_city': 1,
         'ret_from_diff_city': False,
         'ret_to_diff_city': False
     }
     params_2 = {
-        'fly_from': json["Origin"][2],
-        'date_from': json["Departure_d"][2],
-        'date_to': json["Departure_d"][2],
-        'dtime_from': json["Departures_h"][2],
+        'fly_from': json["Origin"][1],
+        'date_from': json["Departure_d"][1],
+        'date_to': json["Departure_d"][1],
+        'dtime_from': json["Departures_h"][1],
         'dtime_to': "23:59",
-        'return_from': json["Arrivals_d"][2],
-        'return_to': json["Arrivals_d"][2],
+        'return_from': json["Arrivals_d"][1],
+        'return_to': json["Arrivals_d"][1],
         'ret_atime_from': "00:00",
-        'ret_atime_to': json["Arrivals_h"][2],
-        'adults': json["Travelers"][2],
-        'max_stopovers': json["Origin"][2],
+        'ret_atime_to': json["Arrivals_h"][1],
+        'adults': json["Travelers"][1],
+        'max_stopovers': json["Origin"][1],
         'one_for_city': 1,
         'ret_from_diff_city': False,
         'ret_to_diff_city': False
@@ -60,32 +60,32 @@ def searcher(json):
     from datetime import datetime
 
      #Origin_1_from
-    original_date_string = f"{json['Departure_d'][1]}T{json['Departures_h'][1]}:00.00Z"
+    original_date_string = f"{json['Departure_d'][0]}T{json['Departures_h'][0]}:00.00Z"
     original_datetime = datetime.strptime(original_date_string, "%d/%m/%YT%H:%M:%S.%fZ")
     new_date_format = "%Y-%m-%dT%H:%M:%S.%fZ"
     time_from_1 = original_datetime.strftime(new_date_format)
 
      #Origin_1_to
-    original_date_string = f"{json['Arrivals_d'][1]}T{json['Arrivals_h'][1]}:00.00Z"
+    original_date_string = f"{json['Arrivals_d'][0]}T{json['Arrivals_h'][0]}:00.00Z"
     original_datetime = datetime.strptime(original_date_string, "%d/%m/%YT%H:%M:%S.%fZ")
     new_date_format = "%Y-%m-%dT%H:%M:%S.%fZ"
     time_to_1 = original_datetime.strftime(new_date_format)
 
      #Origin_2_from
-    original_date_string = f"{json['Departure_d'][2]}T{json['Departures_h'][2]}:00.00Z"
+    original_date_string = f"{json['Departure_d'][1]}T{json['Departures_h'][1]}:00.00Z"
     original_datetime = datetime.strptime(original_date_string, "%d/%m/%YT%H:%M:%S.%fZ")
     new_date_format = "%Y-%m-%dT%H:%M:%S.%fZ"
     time_from_2 = original_datetime.strftime(new_date_format)
 
      #Origin_2_to
-    original_date_string = f"{json['Arrivals_d'][2]}T{json['Arrivals_h'][2]}:00.00Z"
+    original_date_string = f"{json['Arrivals_d'][1]}T{json['Arrivals_h'][1]}:00.00Z"
     original_datetime = datetime.strptime(original_date_string, "%d/%m/%YT%H:%M:%S.%fZ")
     new_date_format = "%Y-%m-%dT%H:%M:%S.%fZ"
     time_to_2 = original_datetime.strftime(new_date_format)
 
     # AÑADIMOS un vuelo nulo con el mismo destino que el origen para cada respuesta.
-    data_1['data'].append({"id": "null", "flyFrom": f"{json['Origin'][1]}", "flyTo": f"{json['Origin'][1]}", "cityFrom": f"{json['Origin'][1]}",
-                           "cityCodeFrom": f"{json['Origin'][1]}", "cityTo": f"{json['Origin'][1]}", "cityCodeTo": f"{json['Origin'][1]}",
+    data_1['data'].append({"id": "null", "flyFrom": f"{json['Origin'][0]}", "flyTo": f"{json['Origin'][0]}", "cityFrom": f"{json['Origin'][0]}",
+                           "cityCodeFrom": f"{json['Origin'][0]}", "cityTo": f"{json['Origin'][0]}", "cityCodeTo": f"{json['Origin'][0]}",
                            "countryFrom": {"code": "null", "name": "null"},
                            "countryTo": {"code": "null", "name": "null"}, "local_departure": time_from_1,
                            "utc_departure": time_from_1, "local_arrival": time_from_1, "utc_arrival": time_from_1,
@@ -99,16 +99,16 @@ def searcher(json):
                                         "personal_item_length": 999, "personal_item_weight": 999,
                                         "personal_item_width": 999}, "availability": {"seats": 9999},
                            "airlines": ["null"], "route": [
-            {"id": "null", "combination_id": "null", "flyFrom": f"{json['Origin'][1]}", "flyTo": f"{json['Origin'][1]}",
-             "cityFrom": f"{json['Origin'][1]}", "cityCodeFrom": f"{json['Origin'][1]}", "cityTo": f"{json['Origin'][1]}",
-             "cityCodeTo": f"{json['Origin'][1]}", "local_departure": time_from_1, "utc_departure": time_from_1,
+            {"id": "null", "combination_id": "null", "flyFrom": f"{json['Origin'][0]}", "flyTo": f"{json['Origin'][0]}",
+             "cityFrom": f"{json['Origin'][0]}", "cityCodeFrom": f"{json['Origin'][0]}", "cityTo": f"{json['Origin'][0]}",
+             "cityCodeTo": f"{json['Origin'][0]}", "local_departure": time_from_1, "utc_departure": time_from_1,
              "local_arrival": time_from_1, "utc_arrival": time_from_1, "airline": "N/A", "flight_no": 999,
              "operating_carrier": "null", "operating_flight_no": "999", "fare_basis": "null", "fare_category": "M",
              "fare_classes": "", "return": 0, "bags_recheck_required": False, "vi_connection": False,
              "guarantee": False, "equipment": 'null', "vehicle_type": "aircraft"},
-            {"id": "null", "combination_id": "null", "flyFrom": f"{json['Origin'][1]}", "flyTo": f"{json['Origin'][1]}",
-             "cityFrom": f"{json['Origin'][1]}", "cityCodeFrom": f"{json['Origin'][1]}", "cityTo": f"{json['Origin'][1]}",
-             "cityCodeTo": f"{json['Origin'][1]}", "local_departure": time_to_1, "utc_departure": time_to_1,
+            {"id": "null", "combination_id": "null", "flyFrom": f"{json['Origin'][0]}", "flyTo": f"{json['Origin'][0]}",
+             "cityFrom": f"{json['Origin'][0]}", "cityCodeFrom": f"{json['Origin'][0]}", "cityTo": f"{json['Origin'][0]}",
+             "cityCodeTo": f"{json['Origin'][0]}", "local_departure": time_to_1, "utc_departure": time_to_1,
              "local_arrival": time_to_1, "utc_arrival": time_to_1, "airline": "null", "flight_no": 999,
              "operating_carrier": "null", "operating_flight_no": "null", "fare_basis": "null", "fare_category": "null",
              "fare_classes": "", "return": 1, "bags_recheck_required": False, "vi_connection": False,
@@ -116,8 +116,8 @@ def searcher(json):
                            "facilitated_booking_available": True, "pnr_count": 2, "has_airport_change": False,
                            "technical_stops": 0, "throw_away_ticketing": False, "hidden_city_ticketing": False,
                            "virtual_interlining": False})
-    data_2['data'].append({"id": "null", "flyFrom": f"{json['Origin'][2]}", "flyTo": f"{json['Origin'][2]}", "cityFrom": f"{json['Origin'][2]}",
-                           "cityCodeFrom": f"{json['Origin'][2]}", "cityTo": f"{json['Origin'][2]}", "cityCodeTo": f"{json['Origin'][2]}",
+    data_2['data'].append({"id": "null", "flyFrom": f"{json['Origin'][1]}", "flyTo": f"{json['Origin'][1]}", "cityFrom": f"{json['Origin'][1]}",
+                           "cityCodeFrom": f"{json['Origin'][1]}", "cityTo": f"{json['Origin'][1]}", "cityCodeTo": f"{json['Origin'][1]}",
                            "countryFrom": {"code": "null", "name": "null"},
                            "countryTo": {"code": "null", "name": "null"}, "local_departure": time_from_2,
                            "utc_departure": time_from_2, "local_arrival": time_from_2, "utc_arrival": time_from_2,
@@ -131,16 +131,16 @@ def searcher(json):
                                         "personal_item_length": 999, "personal_item_weight": 999,
                                         "personal_item_width": 999}, "availability": {"seats": 9999},
                            "airlines": ["null"], "route": [
-            {"id": "null", "combination_id": "null", "flyFrom": f"{json['Origin'][2]}", "flyTo": f"{json['Origin'][2]}",
-             "cityFrom": f"{json['Origin'][2]}", "cityCodeFrom": f"{json['Origin'][2]}", "cityTo": f"{json['Origin'][2]}",
-             "cityCodeTo": f"{json['Origin'][2]}", "local_departure": time_from_2, "utc_departure": time_from_2,
+            {"id": "null", "combination_id": "null", "flyFrom": f"{json['Origin'][1]}", "flyTo": f"{json['Origin'][1]}",
+             "cityFrom": f"{json['Origin'][1]}", "cityCodeFrom": f"{json['Origin'][1]}", "cityTo": f"{json['Origin'][1]}",
+             "cityCodeTo": f"{json['Origin'][1]}", "local_departure": time_from_2, "utc_departure": time_from_2,
              "local_arrival": time_from_2, "utc_arrival": time_from_2, "airline": "N/A", "flight_no": 999,
              "operating_carrier": "null", "operating_flight_no": "999", "fare_basis": "null", "fare_category": "M",
              "fare_classes": "", "return": 0, "bags_recheck_required": False, "vi_connection": False,
              "guarantee": False, "equipment": 'null', "vehicle_type": "aircraft"},
-            {"id": "null", "combination_id": "null", "flyFrom": f"{json['Origin'][2]}", "flyTo": f"{json['Origin'][2]}",
-             "cityFrom": f"{json['Origin'][2]}", "cityCodeFrom": f"{json['Origin'][2]}", "cityTo": f"{json['Origin'][2]}",
-             "cityCodeTo": f"{json['Origin'][2]}", "local_departure": time_to_2, "utc_departure": time_to_2,
+            {"id": "null", "combination_id": "null", "flyFrom": f"{json['Origin'][1]}", "flyTo": f"{json['Origin'][1]}",
+             "cityFrom": f"{json['Origin'][1]}", "cityCodeFrom": f"{json['Origin'][1]}", "cityTo": f"{json['Origin'][1]}",
+             "cityCodeTo": f"{json['Origin'][1]}", "local_departure": time_to_2, "utc_departure": time_to_2,
              "local_arrival": time_to_2, "utc_arrival": time_to_2, "airline": "null", "flight_no": 999,
              "operating_carrier": "null", "operating_flight_no": "null", "fare_basis": "null", "fare_category": "null",
              "fare_classes": "", "return": 1, "bags_recheck_required": False, "vi_connection": False,
@@ -157,13 +157,13 @@ def searcher(json):
 
     for objeto in range(len(data_1['data'])):
         if data_1['data'][objeto]['availability']['seats'] is not None:
-            if data_1['data'][objeto]['availability']['seats'] >= json["Travelers"][1]:
+            if data_1['data'][objeto]['availability']['seats'] >= json["Travelers"][0]:
                 if data_1['data'][objeto]['flyTo'] not in destinos_1:
                     destinos_1.append(data_1['data'][objeto]['flyTo'])
 
     for objeto in range(len(data_2['data'])):
         if data_2['data'][objeto]['availability']['seats'] is not None:
-            if data_2['data'][objeto]['availability']['seats'] >= json["Travelers"][2]:
+            if data_2['data'][objeto]['availability']['seats'] >= json["Travelers"][1]:
                 if data_2['data'][objeto]['flyTo'] not in destinos_2:
                     destinos_2.append(data_2['data'][objeto]['flyTo'])
     possible = list(set(destinos_1) & set(destinos_2))
@@ -191,7 +191,7 @@ def searcher(json):
 
         # 2. Calculo del precio total, precio por viajero y el tiempo de vuelo medio por viajero
 
-        total_passengers = json["Travelers"][1] + json["Travelers"][2]
+        total_passengers = json["Travelers"][0] + json["Travelers"][1]
         total_price = flights[n]['price'] + flights[n + 1]['price']
         price_per_person = (flights[n]['price'] + flights[n + 1]['price']) / total_passengers
         avg_flying_time = (flights[n]['duration']['total'] + flights[n + 1]['duration']['total']) / 2
@@ -226,7 +226,7 @@ def searcher(json):
                   'avg_flying_time': avg_flying_time}
 
         results.append(option)
-        n = n + 2
+        n = n + 2 #number of origins
     results = sorted(results, key=lambda x: x['Total Price'], reverse=False)
 
     df = pd.DataFrame(results)
